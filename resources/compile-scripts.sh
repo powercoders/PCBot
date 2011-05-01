b2db54ecc825dee8c196fcc533c1a94e24766399
Nunnsy <d.nunnzy@live.com>
2011-05-01 16:55:01 -0700

    Made to work with Mac without errors. (powerbot: Nunnsy)

--- resources/compile-scripts.sh
+++ resources/compile-scripts.sh
@@ -1,33 +1,21 @@
-#!/bin/sh
-
-cc=javac
-cflags=
-scripts=Scripts/Sources
-scriptspre=Scripts/Precompiled
-jarpathfile=Settings/path.txt
-
 cd ~/RSBot
-
-if [ ! -e "$jarpathfile" ]; then
+if [ ! -e "Settings/path.txt" ]; then
 	echo "Path file does not exist. Please run RSBot and try again."
 	exit
 fi
-
-for file in $scripts/*.java
+for file in Scripts/Sources/*.java
 do
     if [ ! -e "${file}" ]; then
     	echo "No .java script source files found."
     	exit
     fi
 done
-
 echo "Compiling scripts"
-for file in $scripts/*.class
+for file in Scripts/Sources/*.class
 do
     if [ -e "${file}" ]; then
-    	rm -R $scripts/*.class
+    	rm -R Scripts/Sources/*.class
     	break
     fi
 done
-
-"$cc" $cflags -cp "/$(cat $jarpathfile)" $scripts/*.java
+"javac" -cp "/$(cat Settings/path.txt)" Scripts/Sources/*.java
