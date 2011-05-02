b0638c2ae6fd2c77b4b4a3b7e47fcfa5c7e49a58
Luke Skywalker <aut0r@powerbot.org>
2011-05-01 22:35:40 -0500

    Revert Fixed compile-scripts.sh for mac users

--- resources/compile-scripts.sh
+++ resources/compile-scripts.sh
@@ -1,21 +1,33 @@
+#!/bin/sh
+
+cc=javac
+cflags=
+scripts=Scripts/Sources
+scriptspre=Scripts/Precompiled
+jarpathfile=Settings/path.txt
+
 cd ~/RSBot
-if [ ! -e "Settings/path.txt" ]; then
+
+if [ ! -e "$jarpathfile" ]; then
 echo "Path file does not exist. Please run RSBot and try again."
 exit
 fi
-for file in Scripts/Sources/*.java
+
+for file in $scripts/*.java
 do
 if [ ! -e "${file}" ]; then
 echo "No .java script source files found."
      exit
 fi
 done
+
 echo "Compiling scripts"
-for file in Scripts/Sources/*.class
+for file in $scripts/*.class
 do
 if [ -e "${file}" ]; then
-rm -R Scripts/Sources/*.class
+rm -R $scripts/*.class
      break
 fi
 done
-"javac" -cp "/$(cat Settings/path.txt)" Scripts/Sources/*.java
+
+"$cc" $cflags -cp "/$(cat $jarpathfile)" $scripts/*.java
