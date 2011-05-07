847b5c20027d8e71d2624a67b6ddfc9f9d5eafea
Joe <Timer150@Gmail.com>
2011-05-06 21:52:29 -0400

    Fail eclipse fixes.

--- src/org/rsbot/util/PaintUIUtil.java
+++ src/org/rsbot/util/PaintUIUtil.java
@@ -15,23 +15,18 @@ public class PaintUIUtil {
 	 *
 	 * @param boxRect   Rect to draw checkBox at
 	 * @param text      Text to draw after checkBox
-	 * @param checked   Is checkBox checked
 	 * @param textColor Text color
 	 * @author jtryba
 	 */
-	public void drawCheckBox(final Rectangle boxRect, final String text,
-	                         final Color textColor) {
+	public void drawCheckBox(final Rectangle boxRect, final String text, final Color textColor) {
 		g2.setColor((checked ? Color.green : Color.red));
 		g2.fillRect(boxRect.x, boxRect.y, boxRect.width, boxRect.height);
 		g2.setColor(textColor);
-		g2.drawString(text, boxRect.x + boxRect.width + 3, boxRect.y
-				+ boxRect.height - 2);
+		g2.drawString(text, boxRect.x + boxRect.width + 3, boxRect.y + boxRect.height - 2);
 		g2.setColor(Color.black);
 		if (checked) {
-			g2.drawLine(boxRect.x, boxRect.y, boxRect.x + boxRect.width,
-					boxRect.y + boxRect.height);
-			g2.drawLine(boxRect.x + boxRect.width, boxRect.y, boxRect.x,
-					boxRect.y + boxRect.height);
+			g2.drawLine(boxRect.x, boxRect.y, boxRect.x + boxRect.width, boxRect.y + boxRect.height);
+			g2.drawLine(boxRect.x + boxRect.width, boxRect.y, boxRect.x, boxRect.y + boxRect.height);
 		}
 		g2.drawRect(boxRect.x, boxRect.y, boxRect.width, boxRect.height);
 	}
