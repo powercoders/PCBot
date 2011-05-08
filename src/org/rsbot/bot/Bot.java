7b18911629b22c0db64ed5429871693dff7ade7f
Paris <paris@powerbot.org>
2011-05-08 20:45:02 +0100

    Tidied imports.

--- src/org/rsbot/bot/Bot.java
+++ src/org/rsbot/bot/Bot.java
@@ -1,5 +1,14 @@
 package org.rsbot.bot;
 
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Constructor;
+import java.util.EventListener;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.rsbot.Application;
 import org.rsbot.client.Client;
 import org.rsbot.client.input.Canvas;
@@ -16,14 +25,6 @@ import org.rsbot.script.methods.MethodContext;
 import org.rsbot.script.passives.BankMonitor;
 import org.rsbot.script.passives.WebData;
 import org.rsbot.script.passives.WebLoader;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.lang.reflect.Constructor;
-import java.util.EventListener;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.logging.Logger;
 
 public class Bot {
 	private String account;
