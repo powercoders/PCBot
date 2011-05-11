9dd6639a58cb2bef806b234f97b0ac8e5a0cf095
icnhzabot <icnhzabotsmail@yahoo.com>
2011-05-12 05:48:20 +0800

    According to Eckyecky, this should stop the NullPointer upon a script's onFinish().

--- src/org/rsbot/util/GlobalConfiguration.java
+++ src/org/rsbot/util/GlobalConfiguration.java
@@ -199,7 +199,7 @@ public class GlobalConfiguration {
 			if (!RUNNING_FROM_JAR) {
 				return null;
 			}
-			return new File(GlobalConfiguration.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
+			return new File(GlobalConfiguration.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath().replace("%20", " ");
 		}
 
 		public static String getUnixHome() {
