94ef8d759783173834dda956bc9b559b9c5329ef
Paris <paris@powerbot.org>
2011-05-14 13:14:29 +0100

    Merged in extraction method.

--- src/org/rsbot/Application.java
+++ src/org/rsbot/Application.java
@@ -5,12 +5,15 @@ import org.rsbot.gui.BotGUI;
 import org.rsbot.log.LogOutputStream;
 import org.rsbot.log.SystemConsoleHandler;
 import org.rsbot.security.RestrictedSecurityManager;
-import org.rsbot.util.Extractor;
 import org.rsbot.util.GlobalConfiguration;
+import org.rsbot.util.io.IOHelper;
 
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,7 +23,7 @@ public class Application {
 	public static void main(final String[] args) {
 		try {
 			bootstrap();
-			new Extractor().run();
+			extractResources();
 			commands(args);
 			System.setSecurityManager(new RestrictedSecurityManager());
 			System.setProperty("java.io.tmpdir", GlobalConfiguration.Paths.getGarbageDirectory());
@@ -43,6 +46,27 @@ public class Application {
 					}
 				}
 			}
+		}
+	}
+
+	public static void extractResources() {
+		final ArrayList<String> extract = new ArrayList<String>(2);
+		if (GlobalConfiguration.getCurrentOperatingSystem() == GlobalConfiguration.OperatingSystem.WINDOWS) {
+			extract.add(GlobalConfiguration.Paths.COMPILE_SCRIPTS_BAT);
+			extract.add(GlobalConfiguration.Paths.COMPILE_FIND_JDK);
+		} else {
+			extract.add(GlobalConfiguration.Paths.COMPILE_SCRIPTS_SH);
+		}
+		for (final String item : extract) {
+			final String path = GlobalConfiguration.Paths.Resources.ROOT + "/" + item;
+			final InputStream in;
+			try {
+				in = GlobalConfiguration.getResourceURL(path).openStream();
+			} catch (IOException ignored) {
+				continue;
+			}
+			final File output = new File(GlobalConfiguration.Paths.getHomeDirectory(), item);
+			IOHelper.write(in, output);
 		}
 	}
 
