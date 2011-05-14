e343c4bda0620da9a2a5bd8d6692cc0f95fb4534
Dunnkers Habinio <dunnkers@hotmail.com>
2011-05-14 19:31:56 +0200

    - Added some familiars to the Familiars list. - Fixed method doDismiss(): it previously got often got stuck in the while loop.

--- src/org/rsbot/script/methods/Summoning.java
+++ src/org/rsbot/script/methods/Summoning.java
@@ -29,114 +29,88 @@ public class Summoning extends MethodProvider {
 
 	/**
 	 * Familiar constants.
-	 *
+	 * 
 	 * @author illusion
 	 */
 	public static enum Familiar {
 
 		SPIRIT_WOLF("Spirit Wolf", 1, 6, 3, "Howl", 0), DREADFOWL("Dreadfowl",
-				4, 4, 3, "Dreadfowl Strike", 0), SPIRIT_SPIDER(
-						"Spirit spider",
-						10, 15, 6, "Egg Spawn", 0), THORNY_SNAIL("Thorny snail", 13,
-								16, 3, "Slime Spray", 3), GRANITE_CRAB("Granite crab", 16, 18,
-										12, "Stony Shell",
-										0), MOSQUITO("Mosquito",
-												17, 12, 3,
-												"Pester",
-												0), DESERT_WYRM(
-														"Desert wyrm", 18, 19, 6,
-														"Electric Lash", 0), SPIRIT_SCORPION("Spirit scorpion", 19, 17,
-																6, "Venom Shot", 0), SPIRIT_TZ_KIH("Spirit Tz-Kih", 22, 18, 6,
-																		"Fireball Assault",
-																		0), ALBINO_RAT("Albino rat", 23,
-																				22, 6,
-																				"Cheese Feast",
-																				0), SPIRIT_KALPHITE(
-																						"Spirit kalphite", 25, 22,
-																						6, "Sandstorm", 6), COMPOST_MOUND("Compost Mound", 28, 24, 12,
-																								"Generate Compost", 0), GIANT_CHINCHOMPA("Giant chinchompa",
-																										29, 31, 3, "Explode",
-																										0), VAMPIRE_BAT(
-																												"Vampire bat", 31, 33, 4,
-																												"Vampire Touch", 0), HONEY_BADGER("Honey badger", 32, 25, 12,
-																														"Insane Ferocity", 0), BEAVER("Beaver", 33, 27, 3, "Multichop",
-																																0), VOID_RAVAGER("Void ravager", 34, 27,
-																																		3, "Call to Arms",
-																																		0), VOID_SHIFTER(
-																																				"Void shifter", 34, 94, 3, "Call to Arms", 0), VOID_SPINNER(
-																																						"Void spinner", 34, 27, 3, "Call to Arms", 0), VOID_TORCHER(
-																																								"Void torcher", 34, 94, 3, "Call to Arms", 0), BRONZE_MINOTAUR(
-																																										"Bronze minotaur", 36, 30, 6, "Bronze Bull Rush", 0), BULL_ANT(
-																																												"Bull ant", 40, 30, 12, "Unburden", 9), MACAW("Macaw", 41, 31,
-																																														12, "Herbcall", 0), EVIL_TURNIP("Evil turnip", 42, 30, 6,
-																																																"Evil Flames",
-																																																0), SPIRIT_COCKATRICE(
-																																																		"Spirit cockatrice", 43,
-																																																		36, 3, "Petrifying Gaze", 0), IRON_MINOTAUR("Iron minotaur",
-																																																				46, 37, 6, "Iron Bull Rush", 0), PYRELORD("Pyrelord", 46,
-																																																						32,
-																																																						6, "Immense Heat",
-																																																						0), MAGPIE(
-																																																								"Magpie", 47, 34, 12,
-																																																								"Thieving Fingers", 0), BLOATED_LEECH("Bloated leech", 49, 34,
-																																																										6, "Blood Drain", 0), SPIRIT_TERRORBIRD("Spirit terrorbird",
-																																																												52, 36, 8, "Tireless Run",
-																																																												12), ABYSSAL_PARASITE(
-																																																														"Abyssal parasite", 54, 30, 6, "Abyssal Drain", 7), SPIRIT_JELLY(
-																																																																"Spirit jelly", 55, 43, 6, "Dissolve", 0), IBIS("Ibis", 56, 38,
-																																																																		12, "Fish Rain", 0), STEEL_MINOTAUR("Steel minotaur",
-																																																																				56, 46,
-																																																																				6,
-																																																																				"Steel Bull Rush",
-																																																																				0), SPIRIT_GRAAHK(
-																																																																						"Spirit graahk", 57,
-																																																																						49, 3, "Goad", 0), SPIRIT_KYATT("Spirit kyatt", 57, 49, 3,
-																																																																								"Ambush", 0), SPIRIT_LARUPIA("Spirit larupia", 57, 49, 6,
-																																																																										"Rending", 0), KARAMTHULHU_OVERLORD(
-																																																																												"Karamthulhu overlord", 58,
-																																																																												44, 3, "Doomspere Device", 0), SMOKE_DEVIL("Smoke devil", 61,
-																																																																														48, 6, "Dust Cloud", 0), ABYSSAL_LURKER("Abyssal lurker", 62,
-																																																																																41, 20,
-																																																																																"Abyssal Stealth",
-																																																																																7), SPIRIT_COBRA(
-																																																																																		"Spirit cobra", 63,
-																																																																																		56, 3, "Ophidian Incubation", 0), STRANGER_PLANT(
-																																																																																				"Stranger plant", 64, 49, 6, "Poisonous Blast", 0), BARKER_TOAD(
-																																																																																						"Barker toad", 66, 8, 6, "Toad Bark", 0), MITHRIL_MINOTAUR(
-																																																																																								"Mithril minotaur", 66, 55, 6, "Mithril Bull Rush", 0), WAR_TORTOISE(
-																																																																																										"War tortoise", 67, 43, 20, "Testudo", 18), BUNYIP("Bunyip",
-																																																																																												68, 44, 3, "Swallow Whole", 0), FRUIT_BAT(
-																																																																																														"Fruit bat", 69, 45,
-																																																																																														6, "Fruitfall", 0), RAVENOUS_LOCUST("Ravenous locust", 70, 24,
-																																																																																																12, "Famine", 0), ARCTIC_BEAR("Arctic bear", 71, 28, 6,
-																																																																																																		"Arctic Blast", 0), PHOENIX("Phoenix",
-																																																																																																				72, 30,
-																																																																																																				12,
-																																																																																																				"Rise from the Ashes",
-																																																																																																				0), OBSIDIAN_GOLEM(
-																																																																																																						"Obsidian golem", 73,
-																																																																																																						55, 12, "Volcanic Strength", 0), GRANITE_LOBSTER(
-																																																																																																								"Granite lobster", 74, 47, 6, "Crushing Claw", 0), PRAYING_MANTIS(
-																																																																																																										"Praying mantis", 75, 69, 6, "Mantis Strike", 0), ADAMANT_MINOTAUR(
-																																																																																																												"Adamant minotaur", 76, 66, 6, "Adamant Bull Rush", 0), FORGE_REGENT(
-																																																																																																														"Forge regent", 76, 45, 6, "Inferno", 0), TALON_BEAST(
-																																																																																																																"Talon beast", 77, 49, 6, "Deadly Claw", 0), GIANT_ENT(
-																																																																																																																		"Giant ent", 78, 49, 6, "Acorn Missile", 0), FIRE_TITAN(
-																																																																																																																				"Fire titan", 79, 62, 20, "Titan's Constitution", 0), ICE_TITAN(
-																																																																																																																						"Ice titan", 79, 64, 20, "Titan's Constitution", 0), MOSS_TITAN(
-																																																																																																																								"Moss titan", 79, 58, 20, "Titan's Constitution", 0), HYDRA(
-																																																																																																																										"Hydra", 80, 49, 6, "Regrowth", 0), SPIRIT_DAGANNOTH(
-																																																																																																																												"Spirit dagannoth", 83, 57, 6, "Spike Shot", 0), LAVA_TITAN(
-																																																																																																																														"Lava titan", 83, 61, 4, "Ebon Thunder", 0), SWAMP_TITAN(
-																																																																																																																																"Swamp titan", 85, 56, 6, "Swamp Plague", 0), RUNE_MINOTAUR(
-																																																																																																																																		"Rune minotaur", 86, 151, 6, "Rune Bull Rush", 0), UNICORN_STALLION(
-																																																																																																																																				"Unicorn stallion", 88, 54, 20, "Healing Aura", 0), GEYSER_TITAN(
-																																																																																																																																						"Geyser titan", 89, 69, 6, "Boil", 0), WOLPERTIGER(
-																																																																																																																																								"Wolpertiger", 92, 62, 20, "Magic Focus", 0), ABYSSAL_TITAN(
-																																																																																																																																										"Abyssal titan", 93, 32, 6, "Essence Shipment", 7), IRON_TITAN(
-																																																																																																																																												"Iron titan", 95, 60, 12, "Iron Within", 0), PACK_YAK(
-																																																																																																																																														"Pack yak", 96, 58, 12, "Winter Storage", 30), STEEL_TITAN(
-																																																																																																																																																"Steel titan", 99, 64, 12, "Steel of Legends", 0);
+				4, 4, 3, "Dreadfowl Strike", 0), SPIRIT_SPIDER("Spirit spider",
+				10, 15, 6, "Egg Spawn", 0), THORNY_SNAIL("Thorny snail", 13,
+				16, 3, "Slime Spray", 3), GRANITE_CRAB("Granite crab", 16, 18,
+				12, "Stony Shell", 0), MOSQUITO("Mosquito", 17, 12, 3,
+				"Pester", 0), DESERT_WYRM("Desert wyrm", 18, 19, 6,
+				"Electric Lash", 0), SPIRIT_SCORPION("Spirit scorpion", 19, 17,
+				6, "Venom Shot", 0), SPIRIT_TZ_KIH("Spirit Tz-Kih", 22, 18, 6,
+				"Fireball Assault", 0), ALBINO_RAT("Albino rat", 23, 22, 6,
+				"Cheese Feast", 0), SPIRIT_KALPHITE("Spirit kalphite", 25, 22,
+				6, "Sandstorm", 6), COMPOST_MOUND("Compost Mound", 28, 24, 12,
+				"Generate Compost", 0), GIANT_CHINCHOMPA("Giant chinchompa",
+				29, 31, 3, "Explode", 0), VAMPIRE_BAT("Vampire bat", 31, 33, 4,
+				"Vampire Touch", 0), HONEY_BADGER("Honey badger", 32, 25, 12,
+				"Insane Ferocity", 0), BEAVER("Beaver", 33, 27, 3, "Multichop",
+				0), VOID_RAVAGER("Void ravager", 34, 27, 3, "Call to Arms", 0), VOID_SHIFTER(
+				"Void shifter", 34, 94, 3, "Call to Arms", 0), VOID_SPINNER(
+				"Void spinner", 34, 27, 3, "Call to Arms", 0), VOID_TORCHER(
+				"Void torcher", 34, 94, 3, "Call to Arms", 0), BRONZE_MINOTAUR(
+				"Bronze minotaur", 36, 30, 6, "Bronze Bull Rush", 0), BULL_ANT(
+				"Bull ant", 40, 30, 12, "Unburden", 9), MACAW("Macaw", 41, 31,
+				12, "Herbcall", 0), EVIL_TURNIP("Evil turnip", 42, 30, 6,
+				"Evil Flames", 0), SPIRIT_COCKATRICE("Spirit cockatrice", 43,
+				36, 3, "Petrifying Gaze", 0), SPIRIT_GUTHATRICE(
+				"Spirit guthatrice", 43, 36, 3, "Petrifying Gaze", 0), SPIRIT_SARATRICE(
+				"Spirit saratrice", 43, 36, 3, "Petrifying Gaze", 0), SPIRIT_ZAMATRICE(
+				"Spirit zamatrice", 43, 36, 3, "Petrifying Gaze", 0), SPIRIT_PENGATRICE(
+				"Spirit pengatrice", 43, 36, 3, "Petrifying Gaze", 0), SPIRIT_CORAXATRICE(
+				"Spirit coraxatrice", 43, 36, 3, "Petrifying Gaze", 0), SPIRIT_VULATRICE(
+				"Spirit vulatrice", 43, 36, 3, "Petrifying Gaze", 0), IRON_MINOTAUR(
+				"Iron minotaur", 46, 37, 6, "Iron Bull Rush", 0), PYRELORD(
+				"Pyrelord", 46, 32, 6, "Immense Heat", 0), MAGPIE("Magpie", 47,
+				34, 12, "Thieving Fingers", 0), BLOATED_LEECH("Bloated leech",
+				49, 34, 6, "Blood Drain", 0), SPIRIT_TERRORBIRD(
+				"Spirit terrorbird", 52, 36, 8, "Tireless Run", 12), ABYSSAL_PARASITE(
+				"Abyssal parasite", 54, 30, 6, "Abyssal Drain", 7), SPIRIT_JELLY(
+				"Spirit jelly", 55, 43, 6, "Dissolve", 0), IBIS("Ibis", 56, 38,
+				12, "Fish Rain", 0), STEEL_MINOTAUR("Steel minotaur", 56, 46,
+				6, "Steel Bull Rush", 0), SPIRIT_GRAAHK("Spirit graahk", 57,
+				49, 3, "Goad", 0), SPIRIT_KYATT("Spirit kyatt", 57, 49, 3,
+				"Ambush", 0), SPIRIT_LARUPIA("Spirit larupia", 57, 49, 6,
+				"Rending", 0), KARAMTHULHU_OVERLORD("Karamthulhu overlord", 58,
+				44, 3, "Doomspere Device", 0), SMOKE_DEVIL("Smoke devil", 61,
+				48, 6, "Dust Cloud", 0), ABYSSAL_LURKER("Abyssal lurker", 62,
+				41, 20, "Abyssal Stealth", 7), SPIRIT_COBRA("Spirit cobra", 63,
+				56, 3, "Ophidian Incubation", 0), STRANGER_PLANT(
+				"Stranger plant", 64, 49, 6, "Poisonous Blast", 0), BARKER_TOAD(
+				"Barker toad", 66, 8, 6, "Toad Bark", 0), MITHRIL_MINOTAUR(
+				"Mithril minotaur", 66, 55, 6, "Mithril Bull Rush", 0), WAR_TORTOISE(
+				"War tortoise", 67, 43, 20, "Testudo", 18), BUNYIP("Bunyip",
+				68, 44, 3, "Swallow Whole", 0), FRUIT_BAT("Fruit bat", 69, 45,
+				6, "Fruitfall", 0), RAVENOUS_LOCUST("Ravenous locust", 70, 24,
+				12, "Famine", 0), ARCTIC_BEAR("Arctic bear", 71, 28, 6,
+				"Arctic Blast", 0), PHOENIX("Phoenix", 72, 30, 12,
+				"Rise from the Ashes", 0), OBSIDIAN_GOLEM("Obsidian golem", 73,
+				55, 12, "Volcanic Strength", 0), GRANITE_LOBSTER(
+				"Granite lobster", 74, 47, 6, "Crushing Claw", 0), PRAYING_MANTIS(
+				"Praying mantis", 75, 69, 6, "Mantis Strike", 0), ADAMANT_MINOTAUR(
+				"Adamant minotaur", 76, 66, 6, "Adamant Bull Rush", 0), FORGE_REGENT(
+				"Forge regent", 76, 45, 6, "Inferno", 0), TALON_BEAST(
+				"Talon beast", 77, 49, 6, "Deadly Claw", 0), GIANT_ENT(
+				"Giant ent", 78, 49, 6, "Acorn Missile", 0), FIRE_TITAN(
+				"Fire titan", 79, 62, 20, "Titan's Constitution", 0), ICE_TITAN(
+				"Ice titan", 79, 64, 20, "Titan's Constitution", 0), MOSS_TITAN(
+				"Moss titan", 79, 58, 20, "Titan's Constitution", 0), HYDRA(
+				"Hydra", 80, 49, 6, "Regrowth", 0), SPIRIT_DAGANNOTH(
+				"Spirit dagannoth", 83, 57, 6, "Spike Shot", 0), LAVA_TITAN(
+				"Lava titan", 83, 61, 4, "Ebon Thunder", 0), SWAMP_TITAN(
+				"Swamp titan", 85, 56, 6, "Swamp Plague", 0), RUNE_MINOTAUR(
+				"Rune minotaur", 86, 151, 6, "Rune Bull Rush", 0), UNICORN_STALLION(
+				"Unicorn stallion", 88, 54, 20, "Healing Aura", 0), GEYSER_TITAN(
+				"Geyser titan", 89, 69, 6, "Boil", 0), WOLPERTIGER(
+				"Wolpertiger", 92, 62, 20, "Magic Focus", 0), ABYSSAL_TITAN(
+				"Abyssal titan", 93, 32, 6, "Essence Shipment", 7), IRON_TITAN(
+				"Iron titan", 95, 60, 12, "Iron Within", 0), PACK_YAK(
+				"Pack yak", 96, 58, 12, "Winter Storage", 30), STEEL_TITAN(
+				"Steel titan", 99, 64, 12, "Steel of Legends", 0);
 
 		private final String name;
 		private final int requiredLevel;
@@ -146,8 +120,8 @@ public class Summoning extends MethodProvider {
 		private final int bobSpace;
 		private RSNPC npcObject;
 
-		Familiar(final String name, final int requiredLevel, final int time, final int sp,
-				final String scrollName, final int space) {
+		Familiar(final String name, final int requiredLevel, final int time,
+				final int sp, final String scrollName, final int space) {
 			this.name = name;
 			this.requiredLevel = requiredLevel;
 			this.time = time;
@@ -191,7 +165,6 @@ public class Summoning extends MethodProvider {
 		public RSNPC getNPC() {
 			return npcObject;
 		}
-
 	}
 
 	public Summoning(final MethodContext ctx) {
@@ -300,21 +273,20 @@ public class Summoning extends MethodProvider {
 	/**
 	 * Dismisses the familiar from the summoning tab.
 	 *
-	 * @return <tt>true</tt> if the action was performed.
+	 * @author Dunnkers
+	 * @return <tt>true</tt> if the familiar was dismissed.
 	 */
 	public boolean doDismiss() {
-		methods.interfaces.getComponent(INTERFACE_TAB_SUMMONING,
-				INTERFACE_TAB_SUMMONING_CHILD).doAction("Dismiss");
-
-		RSComponent dismissInterface = methods.interfaces.getComponent(228, 2);
-
-		while (!dismissInterface.isValid()) {
-			sleep(random(400, 600));
-
-			dismissInterface = methods.interfaces.getComponent(228, 2);
+		RSComponent confirmInterface = methods.interfaces.getComponent(228, 2);
+		if (confirmInterface.isValid()) {
+			return confirmInterface.doClick();
+		}else {
+			RSComponent dismissInterface = methods.interfaces.getComponent(INTERFACE_TAB_SUMMONING, INTERFACE_TAB_SUMMONING_CHILD);
+			if (dismissInterface != null && dismissInterface.doAction("Dismiss")) {
+				sleep(random(500, 750));
+			}
 		}
-
-		return dismissInterface.doClick();
+		return false;
 	}
 
 	/**
