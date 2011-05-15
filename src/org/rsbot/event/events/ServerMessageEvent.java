80f567342a003dd73dc87bc9501d04469702d024
unknown <Helcast@.(none)>
2011-05-15 11:26:25 -0700

    Re-indented file. Signed-off-by: unknown <Helcast@.(none)>

--- src/org/rsbot/event/events/ServerMessageEvent.java
+++ src/org/rsbot/event/events/ServerMessageEvent.java
@@ -1,35 +1,29 @@
-/*    */ package org.rsbot.event.events;
-/*    */ 
-/*    */ import java.util.EventListener;
-/*    */ import org.rsbot.event.listeners.ServerMessageListener;
-/*    */ 
-/*    */ @Deprecated
-/*    */ public class ServerMessageEvent extends RSEvent
-/*    */ {
-/*    */   private static final long serialVersionUID = -2786472026976811201L;
-/*    */   private final String message;
-/*    */ 
-/*    */   public ServerMessageEvent(String message)
-/*    */   {
-/* 19 */     this.message = message;
-/*    */   }
-/*    */ 
-/*    */   public void dispatch(EventListener el)
-/*    */   {
-/* 24 */     ((ServerMessageListener)el).serverMessageRecieved(this);
-/*    */   }
-/*    */ 
-/*    */   public long getMask()
-/*    */   {
-/* 29 */     return 1536L;
-/*    */   }
-/*    */ 
-/*    */   public String getMessage() {
-/* 33 */     return this.message;
-/*    */   }
-/*    */ }
+package org.rsbot.event.events;
 
-/* Location:           C:\Users\Helcast\Desktop\
- * Qualified Name:     org.rsbot.event.events.ServerMessageEvent
- * JD-Core Version:    0.6.0
- */
+import java.util.EventListener;
+import org.rsbot.event.listeners.ServerMessageListener;
+
+@Deprecated
+public class ServerMessageEvent extends RSEvent
+{
+    private static final long serialVersionUID = -2786472026976811201L;
+    private final String message;
+    public ServerMessageEvent(String message)
+    {
+        this.message = message;
+    }
+
+    public void dispatch(EventListener el)
+    {
+        ((ServerMessageListener)el).serverMessageRecieved(this);
+    }
+
+    public long getMask()
+    {
+        return 1536L;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
