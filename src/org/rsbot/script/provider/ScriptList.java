7899c3a4c86a49e8caa1a81e39f4c36f7ac35f2f
Paris <paris@powerbot.org>
2011-05-24 16:20:30 +0100

    List scripts (for SDN).

---
+++ src/org/rsbot/script/provider/ScriptList.java
@@ -0,0 +1,47 @@
+package org.rsbot.script.provider;
+
+import java.io.File;
+import java.io.PrintStream;
+
+public class ScriptList {
+	public static void main(final String[] args) {
+		if (args.length == 0) {
+			return;
+		}
+		final PrintStream out = System.out;
+		final FileScriptSource source = new FileScriptSource(args[0]);
+		for (final ScriptDefinition item : source.list()) {
+			out.print("[");
+			out.print(new File(item.path).getName());
+			out.println("]");
+			out.print("id=");
+			out.println(Integer.toString(item.id));
+			printValue(out, "name", item.name);
+			out.print("version=");
+			out.println(Double.toString(item.version));
+			printValue(out, "description", item.description);
+			printValue(out, "authors", item.authors);
+			printValue(out, "keywords", item.keywords);
+			printValue(out, "website", item.website);
+		}
+	}
+
+	private static void printValue(final PrintStream out, final String key, final String... texts) {
+		out.print(key);
+		out.print("=");
+		for (int i = 0; i < texts.length; i++) {
+			if (i != 0) {
+				out.print(",");
+			}
+			out.print(stripNewline(texts[i]));
+		}
+		out.println();
+	}
+
+	private static String stripNewline(String text) {
+		text = text.replace("\r\n", " ");
+		text = text.replace("\r", " ");
+		text = text.replace("\n", " ");
+		return text;
+	}
+}
