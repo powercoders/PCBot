30b81684d2acc086a1970fac9675ed63fd656694
Milan <milan@powerbot.org>
2011-06-02 20:17:33 +0200

    * Further model handling optimizing. This saves a lot of resources when running my modscripts. (Won't be available before the next RS update)

--- src/org/rsbot/client/ModelCapture.java
+++ src/org/rsbot/client/ModelCapture.java
@@ -18,12 +18,22 @@ public class ModelCapture implements Model {
 	private short[] face_a;
 	private short[] face_b;
 	private short[] face_c;
+    private int numVertices;
+    private int numFaces;
 
 	public ModelCapture(final Model model) {
 		if (model == null) {
 			return;
 		}
-        update(model);
+        vertex_x = model.getXPoints().clone();
+		vertex_y = model.getYPoints().clone();
+		vertex_z = model.getZPoints().clone();
+        numVertices = Math.min(vertex_x.length, Math.min(vertex_y.length, vertex_z.length));
+
+		face_a = model.getIndices1().clone();
+		face_b = model.getIndices2().clone();
+		face_c = model.getIndices3().clone();
+        numFaces = Math.min(face_a.length, Math.min(face_b.length, face_c.length));
 	}
 
     public static Model updateModel(final Model model, final Model container)
@@ -38,19 +48,44 @@ public class ModelCapture implements Model {
         if (model == null) {
 			return;
 		}
-        int[] vertices = model.getXPoints();
-		vertex_x = Arrays.copyOf(vertices, vertices.length);
-		vertices = model.getYPoints();
-		vertex_y = Arrays.copyOf(vertices, vertices.length);
-		vertices = model.getZPoints();
-		vertex_z = Arrays.copyOf(vertices, vertices.length);
 
-		short[] faces = model.getIndices1();
-		face_a = Arrays.copyOf(faces, faces.length);
-		faces = model.getIndices2();
-		face_b = Arrays.copyOf(faces, faces.length);
-		faces = model.getIndices3();
-		face_c = Arrays.copyOf(faces, faces.length);
+        int[] vertices_x = model.getXPoints();
+        int[] vertices_y = model.getYPoints();
+        int[] vertices_z = model.getZPoints();
+        short[] indices1 = model.getIndices1();
+        short[] indices2 = model.getIndices2();
+        short[] indices3 = model.getIndices3();
+        int numVertices = Math.min(vertices_x.length, Math.min(vertices_y.length, vertices_z.length));
+        int numFaces = Math.min(indices1.length, Math.min(indices2.length, indices3.length));
+        if(numVertices > this.numVertices)
+        {
+            this.numVertices = numVertices;
+            vertex_x = vertices_x.clone();
+            vertex_y = vertices_y.clone();
+            vertex_z = vertices_z.clone();
+        }
+        else
+        {
+            this.numVertices = numVertices;
+            System.arraycopy(vertices_x, 0, vertex_x,0, numVertices);
+            System.arraycopy(vertices_z, 0, vertex_z,0, numVertices);
+            System.arraycopy(vertices_z, 0, vertex_z,0, numVertices);
+        }
+
+        if(numFaces > this.numFaces)
+        {
+            this.numFaces = numFaces;
+            face_a = indices1.clone();
+            face_b = indices2.clone();
+            face_c = indices3.clone();
+        }
+        else
+        {
+            this.numFaces = numFaces;
+            System.arraycopy(indices1, 0, face_a, 0, numFaces);
+            System.arraycopy(indices2, 0, face_b, 0, numFaces);
+            System.arraycopy(indices3, 0, face_c, 0, numFaces);
+        }
     }
 
     @Override
@@ -83,4 +118,11 @@ public class ModelCapture implements Model {
 		return face_c;
 	}
 
+    public int getNumVertices() {
+        return numVertices;
+    }
+
+    public int getNumFaces() {
+        return numFaces;
+    }
 }
--- src/org/rsbot/script/wrappers/RSCharacterModel.java
+++ src/org/rsbot/script/wrappers/RSCharacterModel.java
@@ -16,8 +16,8 @@ class RSCharacterModel extends RSModel {
 		this.c = c;
 		x_base = xPoints;
 		z_base = zPoints;
-		xPoints = new int[xPoints.length];
-		zPoints = new int[zPoints.length];
+		xPoints = new int[numVertices];
+		zPoints = new int[numVertices];
 	}
 
 	/**
