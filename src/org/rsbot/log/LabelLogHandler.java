a59a6e3667f67543d7dab51fc99a684f3d0ba7d9
Paris <paris@powerbot.org>
2011-06-02 23:39:15 +0100

    Removed use of deprecated FontManager API.

--- src/org/rsbot/log/LabelLogHandler.java
+++ src/org/rsbot/log/LabelLogHandler.java
@@ -1,10 +1,7 @@
 package org.rsbot.log;
 
-import sun.font.FontManager;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -16,15 +13,6 @@ public class LabelLogHandler extends Handler {
 
 	public LabelLogHandler() {
 		super();
-		final String pref = "Segoe UI";
-
-		String[] fontNames = FontManager.getFontNamesFromPlatform();
-		if (fontNames != null) {
-			if (Arrays.asList(fontNames).contains(pref)) {
-				final Font font = label.getFont();
-				label.setFont(new Font(pref, font.getStyle(), font.getSize()));
-			}
-		}
 		defaultColor = label.getForeground();
 	}
 
