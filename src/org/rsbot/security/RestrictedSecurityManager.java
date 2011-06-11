412c6b9943c046ddcb44e42a3c46e6609eb119d6
Paris <paris@powerbot.org>
2011-06-11 08:11:00 +0100

    Block pastebins.

--- src/org/rsbot/security/RestrictedSecurityManager.java
+++ src/org/rsbot/security/RestrictedSecurityManager.java
@@ -68,8 +68,6 @@ public class RestrictedSecurityManager extends SecurityManager {
 				".wikia.com",
 				".wikia.nocookie.net",
 
-				"pastie.org",
-				"pastebin.com",
 				"shadowscripting.org", // iDungeon
 				"shadowscripting.wordpress.com", // iDungeon
 				".glorb.nl", // SXForce - Swamp Lizzy Paid, Snake Killah
