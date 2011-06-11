e0b2e04b3aee3ca0a4781d9ff9e1d53aeb326b7b
Paris <paris@powerbot.org>
2011-06-11 18:09:07 +0100

    Use protection domain with permissions.

--- src/org/rsbot/script/provider/ScriptClassLoader.java
+++ src/org/rsbot/script/provider/ScriptClassLoader.java
@@ -1,19 +1,54 @@
 package org.rsbot.script.provider;
 
+import java.awt.AWTPermission;
 import java.io.ByteArrayOutputStream;
+import java.io.FilePermission;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.SocketPermission;
 import java.net.URL;
+import java.security.CodeSigner;
+import java.security.CodeSource;
+import java.security.Permissions;
+import java.security.ProtectionDomain;
+import java.util.PropertyPermission;
+
+import org.rsbot.Configuration;
+import org.rsbot.security.RestrictedSecurityManager;
 
 /**
  */
 class ScriptClassLoader extends ClassLoader {
-
+	private final ProtectionDomain domain;
 	private final URL base;
 
 	public ScriptClassLoader(final URL url) {
 		base = url;
+		final CodeSource codeSource = new CodeSource(base, (CodeSigner[]) null);
+		domain = new ProtectionDomain(codeSource, getPermissions());
+	}
+
+	private Permissions getPermissions() {
+		final Permissions ps = new Permissions();
+		ps.add(new AWTPermission("accessEventQueue"));
+		ps.add(new PropertyPermission("user.home", "read"));
+		ps.add(new PropertyPermission("java.vendor", "read"));
+		ps.add(new PropertyPermission("java.version", "read"));
+		ps.add(new PropertyPermission("os.name", "read"));
+		ps.add(new PropertyPermission("os.arch", "read"));
+		ps.add(new PropertyPermission("os.version", "read"));
+		ps.add(new SocketPermission("*", "resolve"));
+		for (String host : RestrictedSecurityManager.ALLOWED_HOSTS) {
+			if (host.startsWith(".")) {
+				host = "*" + host;
+			}
+			ps.add(new SocketPermission(host + ":80", "connect,resolve"));
+			ps.add(new SocketPermission(host + ":443", "connect,resolve"));
+		}
+		ps.add(new FilePermission(Configuration.Paths.getScriptCacheDirectory(), "read,write,delete"));
+		ps.setReadOnly();
+		return ps;
 	}
 
 	@Override
@@ -31,7 +66,7 @@ class ScriptClassLoader extends ClassLoader {
 					out.write(buffer, 0, n);
 				}
 				final byte[] bytes = out.toByteArray();
-				clazz = defineClass(name, bytes, 0, bytes.length);
+				clazz = defineClass(name, bytes, 0, bytes.length, domain);
 				if (resolve) {
 					resolveClass(clazz);
 				}
