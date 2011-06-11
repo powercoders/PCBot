02c1e12e42f8ba3f0dd08deebd856d7a5a909923
Johan Bergman <johan.bergman1@gmail.com>
2011-06-11 12:48:38 +0200

    Fixed minor Swedish grammar, standardized menu names.

--- src/org/rsbot/locale/Messages_sv.java
+++ src/org/rsbot/locale/Messages_sv.java
@@ -3,6 +3,7 @@ package org.rsbot.locale;
 /**
  * @author DARW1N
  * @author ChalamiuS
+ * @author CCSki
  */
 public class Messages_sv extends Messages {
 
@@ -12,40 +13,40 @@ public class Messages_sv extends Messages {
 
 	public final String LANGUAGE = "Svenska";
 
-	public final String FILE = "Fil";
+	public final String FILE = "Arkiv";
 	public final String EDIT = "Redigera";
-	public final String VIEW = "Vy";
+	public final String VIEW = "Visa";
 	public final String TOOLS = "Verktyg";
-	public final String HELP = "HjÃ¤lp";
+	public final String HELP = "Hjälp";
 
 	public final String NEWBOT = "Ny bot";
-	public final String CLOSEBOT = "StÃ¤ng Bot";
-	public final String HIDEBOT = "DÃ¶lj";
-	public final String ADDSCRIPT = "LÃ¤gg till Script";
-	public final String RUNSCRIPT = "KÃ¶r Script";
-	public final String RESUMESCRIPT = "Ã…teruppta Script";
-	public final String STOPSCRIPT = "Stoppa Script";
-	public final String PAUSESCRIPT = "Pausa Script";
-	public final String SAVESCREENSHOT = "SkÃ¤rmdump";
+	public final String CLOSEBOT = "Stäng bot";
+	public final String HIDEBOT = "Dölj";
+	public final String ADDSCRIPT = "Lägg till script";
+	public final String RUNSCRIPT = "Kör script";
+	public final String RESUMESCRIPT = "Återuppta script";
+	public final String STOPSCRIPT = "Stoppa script";
+	public final String PAUSESCRIPT = "Pausa script";
+	public final String SAVESCREENSHOT = "Skärmdump";
 	public final String EXIT = "Avsluta";
 
 	public final String ACCOUNTS = "Konton";
 	public final String FORCEINPUT = "Tvinga inmatning";
-	public final String DISABLEANTIRANDOMS = "Inaktivera Randoms";
-	public final String DISABLEAUTOLOGIN = "Inaktivera Inloggning";
-	public final String DISABLEADS = "Inaktivera Annonser";
-	public final String DISABLECONFIRMATIONS = "Inaktivera BekrÃ¤ftelser";
-	public final String BINDTO = "Bind till Adress:";
-	public final String USEPASSWORD = "AnvÃ¤nd lÃ¶senord:";
+	public final String DISABLEANTIRANDOMS = "Inaktivera randoms";
+	public final String DISABLEAUTOLOGIN = "Inaktivera inloggning";
+	public final String DISABLEADS = "Inaktivera annonser";
+	public final String DISABLECONFIRMATIONS = "Inaktivera bekräftelser";
+	public final String BINDTO = "Bind till adress:";
+	public final String USEPASSWORD = "Använd lösenord:";
 	public final String LESSCPU = "Mindre CPU";
-	public final String DISABLECANVAS = "Inaktivera Canvas";
-	public final String EXTDVIEWS = "UtÃ¶kad visningar";
-	public final String AUTOSHUTDOWN = "Automatisk avstÃ¤ngning (mins):";
-	public final String BETAPATCH = "AnvÃ¤nd beta client patch";
+	public final String DISABLECANVAS = "Inaktivera canvas";
+	public final String EXTDVIEWS = "Utökad visa-meny";
+	public final String AUTOSHUTDOWN = "Automatisk avstängning (min):";
+	public final String BETAPATCH = "Använd beta client patch";
 
-	public final String HIDETOOLBAR = "DÃ¶lj VerktygsfÃ¤lt";
-	public final String HIDELOGPANE = "DÃ¶lj Logga Panel";
-	public final String ALLDEBUGGING = "Alla Debugging";
+	public final String HIDETOOLBAR = "Dölj verktygsfält";
+	public final String HIDELOGPANE = "Dölj loggpanel";
+	public final String ALLDEBUGGING = "All felsökning";
 
 	public final String CLEARCACHE = "Rensa cache";
 	public final String OPTIONS = "Alternativ";
@@ -53,6 +54,11 @@ public class Messages_sv extends Messages {
 	public final String SITE = "Hemsida";
 	public final String PROJECT = "Projekt";
 	public final String ABOUT = "Om oss";
+	
+	public final String TOGGLE = "Toggle";
+	public final String TOGGLEFALSE = TOGGLE + "F ";
+	public final String TOGGLETRUE = TOGGLE + "T ";
+	public final String MENUSEPERATOR = "-";
 
 	public final String TABDEFAULTTEXT = "Bot";
 }
